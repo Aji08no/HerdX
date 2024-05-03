d30ce79b966a417091cd60d24c1cf5a041654456
Aji <qaqc11no2022@gmail.com>
2024-05-03 13:06:01 +0530

    AllDeleteTag module in under transfer script is done

--- src/test/java/Part/AllDeleteTag.java
+++ src/test/java/Part/AllDeleteTag.java
@@ -15,6 +15,9 @@ import java.util.Random;
 public class AllDeleteTag extends Herd {
     WebDriverWait wait;
     WebDriverWait wait1;
+    String transferPenName="";
+
+    String penNameActual="";
 
     @Test(priority = 0)
     public void deleteAllPenTags() throws InterruptedException {
@@ -30,6 +33,7 @@ public class AllDeleteTag extends Herd {
             WebElement penContent = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//android.view.View[@index='1'])[4]")));
             String content2 = penContent.getAttribute("content-desc");
             String[] lines2 = content2.split("\\r?\\n");
+            penNameActual=lines2[0];
             System.out.println("PenName : " + lines2[0]);
             System.out.println("No.of PenTags : " + lines2[1]);
             if (penContent.isDisplayed()) {
@@ -94,6 +98,7 @@ public class AllDeleteTag extends Herd {
         WebElement penContent = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.view.View[@index='1'])[4]")));
         String content2 = penContent.getAttribute("content-desc");
         String[] lines2 = content2.split("\\r?\\n");
+        penNameActual=lines2[0];
         String totalTag = lines2[1];
         if (Integer.parseInt(totalTag) > 3) {
             penContent.click();
@@ -163,6 +168,7 @@ public class AllDeleteTag extends Herd {
                 Random r = new Random();
                 int rNumber = r.nextInt(100, 999);
                 enter.sendKeys("2" + rNumber);
+                driver.hideKeyboard();
             }
             Thread.sleep(1500);
 
@@ -172,14 +178,33 @@ public class AllDeleteTag extends Herd {
                 if (viewButton.isEnabled()) {
                     viewButton.click();
                 } else {
-                    //enter tag
-                    Thread.sleep(500);
-                    WebElement enter2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[@index='1'])[3]")));
-                    enter2.click();
-                    enter2.clear();
-                    Random r2 = new Random();
-                    int rNumber2 = r2.nextInt(100, 999);
-                    enter2.sendKeys("2" + rNumber2);
+
+                        boolean condition = false;
+                        while (!condition){
+                            try{
+                                String summeryView = viewButton.getAttribute("enabled");
+                                if (summeryView.equals("false")){
+                                    //enter tag
+                                    Thread.sleep(500);
+                                    WebElement enter2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[@index='1'])[3]")));
+                                    enter2.click();
+                                    enter2.clear();
+                                    Random r2 = new Random();
+                                    int rNumber2 = r2.nextInt(100, 999);
+                                    enter2.sendKeys("2" + rNumber2);
+                                    driver.hideKeyboard();
+
+                                }else {
+                                    condition=true;
+                                }
+                            }
+                            catch (Exception e2){
+                                e2.getMessage();
+                            }
+                        }
+                    //view summary
+                    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@content-desc='View Summary']"))).click();
+
                 }
             } catch (Exception e) {
                 System.out.println(e.getMessage());
@@ -203,9 +228,200 @@ public class AllDeleteTag extends Herd {
 
         //menu dot
         Thread.sleep(2000);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
-       //select animals-manual
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@content-desc,'Select Animals - M')]"))).click();
+        try {
+            //menu dot
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
+            WebDriverWait waitBack=new WebDriverWait(driver,Duration.ofSeconds(8));
+            WebElement manual = waitBack.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@content-desc,'Select Animals - M')]")));
+            if (manual.isEnabled()){
+                manual.click();
+            }
+
+        }catch (Exception e){
+            Thread.sleep(200);
+            System.out.println("Tags are under transfer");
+//            driver.pressKey(new KeyEvent(AndroidKey.BACK));
+            //
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Scrim']"))).click();
+            //back arrow
+            Thread.sleep(200);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.widget.ImageView[@index='0'])[1]"))).click();
+
+            //plus icon create pen
+            wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.widget.ImageView[@index='1'])[2]"))).click();
+            //add pen/pasture
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Add Pen / Pasture']"))).click();
+            //enter add pasture
+            Random ran=new Random();
+            int outR = ran.nextInt(0,10);
+            WebElement enter = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@index='2']")));
+            enter.click();
+            String penName = "0aa"+outR;
+            transferPenName=penName;
+            Thread.sleep(200);
+            enter.sendKeys(transferPenName);
+            driver.hideKeyboard();
+            //pen
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Pen']"))).click();
+            //save
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Save']"))).click();
+            //try
+            try {
+                wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@content-desc,'Added Successfully')]")));
+            }
+            catch (Exception e1){
+                boolean addCondition = true;
+                while (addCondition){
+                    WebElement editText = wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.EditText[@index='2']")));
+                    if (editText.isDisplayed()){
+                        editText.click();
+                        Thread.sleep(200);
+                        editText.clear();
+                        Random r=new Random();
+                        int rNumber = r.nextInt(0, 99);
+                        Thread.sleep(100);
+                        String penName2 = "0aa"+outR;
+                        transferPenName=penName2;
+                        Thread.sleep(200);
+                        enter.sendKeys(transferPenName);
+                        driver.hideKeyboard();
+
+                        //pasture
+                        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Pen']"))).click();
+                        //save
+                        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Save']"))).click();
+
+                        try {
+                            wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@content-desc,'Enclosure Name Already Exists')]")));
+                        }catch (Exception e2){
+                            addCondition = false;
+                        }
+
+                    }
+
+                }
+            }
+            //plus icon create inventory
+            Thread.sleep(5000);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.widget.ImageView[@index='1'])[2]"))).click();
+
+            //Add animal to inventory
+            Thread.sleep(500);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@index='0' and @class='android.widget.ImageView']"))).click();
+
+            //Manual Entry
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@content-desc,'Manual Entry')]"))).click();
+            //Manual click
+            Thread.sleep(1000);
+            //click pen/pasture
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Pen / Pasture']"))).click();
+            //right arrow
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
+
+            //select pen/pasture
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[@content-desc='Select'])[1]"))).click();
+            //select pen
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//android.widget.ImageView[@index='0'])[3]"))).click();
+            //done
+            Thread.sleep(200);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Done']"))).click();
+            //right arrow
+            Thread.sleep(200);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
+            //right arrow
+            Thread.sleep(200);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
+
+            try{
+                //EPC Prefix
+                wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@content-desc,'No tags assigned to this organization')]")));
+                System.out.println("No tags assigned to this organization! Please contact HerdX support.");
+                //back arrow
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='0']"))).click();
+
+                //close pop up
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Close']"))).click();
+                driver.pressKey(new KeyEvent(AndroidKey.BACK));
+            } catch (Exception e1) {
+                e1.getMessage();
+            }
+            //EPC Prefix
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Select']"))).click();
+            //EPC Prefix dropdown select
+            Thread.sleep(500);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='E26878434']"))).click();
+            //select
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.widget.Button)[3]"))).click();
+            //select dropDown
+            Thread.sleep(100);
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='99977149481']"))).click();
+            //enter tag
+            Thread.sleep(500);
+            for (int i = 0; i < 4; i++) {
+                WebElement enter1 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[@index='1'])[3]")));
+                enter1.click();
+                enter1.clear();
+                Random r = new Random();
+                int rNumber = r.nextInt(100, 999);
+                enter1.sendKeys("2" + rNumber);
+                driver.hideKeyboard();
+            }
+            Thread.sleep(1500);
+
+            try {
+                //view summary
+                WebElement viewButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='View Summary']")));
+                if (viewButton.isEnabled()) {
+                    viewButton.click();
+                } else {
+                    //enter tag
+                        boolean condition = false;
+                        while (!condition){
+                            try{
+//                                WebElement viewButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='View Summary']")));
+                                String summeryView = viewButton.getAttribute("enabled");
+                                if (summeryView.equals("false")){
+                                    //enter tag
+                                    Thread.sleep(500);
+                                    WebElement enter2 = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//*[@index='1'])[3]")));
+                                    enter2.click();
+                                    enter2.clear();
+                                    Random r2 = new Random();
+                                    int rNumber2 = r2.nextInt(100, 999);
+                                    enter2.sendKeys("2" + rNumber2);
+                                    driver.hideKeyboard();
+
+                                }else {
+                                   condition=true;
+                                }
+                            }
+                            catch (Exception e2){
+                                e2.getMessage();
+                            }
+                        }
+                    viewButton.click();
+                }
+                //add animals to inventory
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Add Animals to Inventory']"))).click();
+                //yes
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Yes']"))).click();
+                //pen content
+                WebElement penContent1 =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("(//android.view.View[@index='1'])[4]")));
+                String content1 = penContent1.getAttribute("content-desc");
+                String[] lines1 = content1.split("\\r?\\n");
+                System.out.println("PenName : " + lines1[0]);
+                System.out.println("No.of PenTag : " + lines1[1]);
+
+                penContent1.click();
+                //menu dot
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
+                //select animals-manual
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@content-desc,'Select Animals - M')]"))).click();
+
+            } catch (Exception e1) {
+                e1.getMessage();
+            }
+        }
         //select all
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Select All']"))).click();
         //quick actions
@@ -218,26 +434,43 @@ public class AllDeleteTag extends Herd {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Next']"))).click();
         //delete
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Delete']"))).click();
-        try {
-            //delete
-            WebDriverWait wait1=new WebDriverWait(driver,Duration.ofSeconds(8));
-            wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Delete']"))).click();
-        }
-        catch (Exception e){
-            e.getMessage();
-        }
+//        try {
+//            //delete
+//            WebDriverWait wait1=new WebDriverWait(driver,Duration.ofSeconds(8));
+//            wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Delete']"))).click();
+//        }
+//        catch (Exception e){
+//            e.getMessage();
+//        }
+
+//        Thread.sleep(2000);
         //delete pop up
-        Thread.sleep(8000);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@content-desc,'Animals Has Been Deleted')]")));
+        WebDriverWait loadWait=new WebDriverWait(driver,Duration.ofSeconds(50));
+        WebElement www = loadWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@content-desc,'Been Deleted')]")));
+        System.out.println(www.getAttribute("content-desc"));
         System.out.println("Successfully tags was deleted");
     }
 
     @Test(priority = 1)
-    public void verifyAllDeletedTags(){
+    public void verifyAllDeletedTags() throws InterruptedException {
         //pen content
         WebElement content = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.view.View[@index='1'])[4]")));
         String verifyContent = content.getAttribute("content-desc");
         String[] lines = verifyContent.split("\\r?\\n");
+        String penNameExpected = lines[0];
+
+        if (penNameExpected.equals(penNameActual)) {
+            content.click();
+        }
+        else {
+            System.out.println("under transfer");
+            //search bar
+            WebElement searchBar = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@index='14']")));
+            searchBar.click();
+            Thread.sleep(200);
+            searchBar.sendKeys(transferPenName);
+            driver.hideKeyboard();
+            }
         System.out.println("After deleted Tags in Pen");
         System.out.println("PenName : "+lines[0]);
         System.out.println("No.of PenTags : "+lines[1]);
