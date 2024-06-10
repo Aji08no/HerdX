a55622050659544adead9296a4298a394f557422
Aji <qaqc11no2022@gmail.com>
2024-06-10 12:40:43 +0530

    Task module addes full script

--- src/test/java/Part/Task.java
+++ src/test/java/Part/Task.java
@@ -1,6 +1,62 @@
 package Part;
 
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class Task extends Herd{
+
+    @Test(priority = 0)
+    public void createTask() throws InterruptedException {
+        Thread.sleep(5000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        TouchAction touchAction = new TouchAction(driver);
+        //tasks
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@content-desc,'Tasks')]"))).click();
+        //plus icon
+        Thread.sleep(25000);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='3']"))).click();
+        //enter task title
+        WebElement enterTask = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@index='3']")));
+        enterTask.click();
+        Thread.sleep(100);
+        enterTask.sendKeys("morning");
+        //enter description
+        WebElement enterDescription = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText[@index='5']")));
+        enterDescription.click();
+        enterDescription.sendKeys("This task is very difficult");
+        driver.hideKeyboard();
+
+        //select assign
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='7']"))).click();
+        //select drop down value
+        Thread.sleep(100);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='You']"))).click();
+        //select location
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.ImageView[@index='9']"))).click();
+        //select asTest
+
+        touchAction.press(PointOption.point(474,2047))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(3)))
+                .moveTo(PointOption.point(469,397))
+                .release()
+                .perform();
+        Thread.sleep(100);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='new']"))).click();
+        //calendar icon
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("(//android.widget.ImageView[@index='0'])[2]"))).click();
+        //done
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Done']"))).click();
+        //save
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@content-desc='Save']"))).click();
+    }
 
 
 }
